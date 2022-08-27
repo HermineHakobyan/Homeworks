b2d037a438dc97d2dfcd39874728c52759bcdb81
HermineHakobyan <106839268+HermineHakobyan@users.noreply.github.com>
2022-08-27 14:56:30 +0400

    "changes"

--- nor.js
+++ nor.js
@@ -2,6 +2,8 @@ const poster = document.querySelector("header img");
 const advs = document.querySelectorAll("#main_promo .mp");
 const filmsBlock = document.getElementById("films");
 const form = document.querySelector("#add");
+const singInModalBtn=document.querySelector("[data-in]");
+const singInModal=document.querySelector("[data-in-modal]");
 
 const _DB = {
 	movies: [
@@ -92,11 +94,89 @@ function removeFilmFromList (selector) {
 createFilmsList(_DB.movies, filmsBlock);
 
 
+singInModalBtn.addEventListener("click", (e) =>{
+	e.preventDefault();
+	singInModal.classList.add("modal_wrapper-active");
+});
+window.addEventListener("mouseup", (e)=>{
+	if(e.target !== singInModal.querySelector("form")){
+	singInModal.classList.remove("modal_wrapper-active");
+	}
+});
 
-// ողջույն տնային աշխատանք 31
+//elem.classList.method()
+//add()=> avelatsnum enq class
+//remove()=> jnjum enq class@
+//toggle()=> class ka chka
+//contains()=> stugum e arkayutyun@ classi ` true\false
+//item()=> veradardznum e classi anun@ @st indexi =. array
+//classlist =>DOMtokenlist =>[]
 
-// 1․ եթե օգտատերը նշում ա որ ֆիլմը ֆավորիտ ա, ապա ֆիլմի անունի գույնը պետք ա փոխվի, այսինքն ցուցակում պետք ա տարբերբեն այն ֆիլմերը որոնք ֆավորիտ են
 
-// 2․ ստեղծում եք ֆունկցիոնալ, որ ցուցակում 20ից ավել ֆիլմ չլինի, բայց ֆիլմերը պահպանվեն տվյալների բազայում, ու ամեն մուտքի ժամանակ ֆիլմերի 
-// անունները պետք ա ռանդոմ ցույց տա էդ 20 հատի մեջ,
-//  եթե 20ից քիչ են ֆիլմերը ապա ռանդոմը չպետք ա աշխատի, ռանդոմի ժամանակ պետք ա նորից սորտավորվեն
+//const some= document.getElementById("some");
+//some.classList.add("first", "second", "third");
+// //some.classList.remove("first", "second", "third");
+// console.log(some.classList.item(0));
+
+
+//nayelu 
+// const clickMe= document.getElementById("clickme");
+// clickMe.addEventListener("click", function (){
+	//this.claasList.toggle("light");
+// 	if(!this.classList.contains("light")){
+// 		this.classList.add("light");
+// 	}else{
+		//this.removeAttribute("class");
+// 		this.classList.remove("light");
+// 	}
+// });
+
+//contains-@ veradardznum e true kam false, aysinqn ayd classn uni te voch
+//console.log(some.classList.contains("first"));
+
+form.addEventListener("submit", (e) => {
+	e.preventDefault();
+	let val = e.target.firstElementChild.value.trim();
+	const check = document.querySelector("input[name='favorite']");
+  
+	if (val !== "" && val !== "<" && val !== ">" && val !== "/" && val !== "?") {
+	  if (check.checked) {
+		console.log(`This film <${val}> added to favorite`);
+	  }
+	  _DB.movies.push(val);
+	}
+  
+	setSort(_DB.movies);
+	createFilmsList(_DB.movies, filmsBlock);
+	e.target.reset();
+  });
+  function setSort(arr) {
+	arr.sort();
+  }
+  function createFilmsList(filmsArr, parent) {
+	parent.innerHTML = "";
+	setSort(filmsArr);
+	filmsArr.forEach((film, index) => {
+	  parent.innerHTML += `
+			  <p>
+				  ${index + 1}. 
+				  ${film.length >= 21 ? film.slice(0, 21) + '...' : film}
+				  <span data-rm>&#128465</span>
+			  </p>
+		  `;
+	});
+  
+	removeFilmFromList('[data-rm]');
+  
+  }
+  function removeFilmFromList(selector) {
+	setSort(_DB.movies);
+	document.querySelectorAll(selector).forEach((btn, index) => {
+	  btn.addEventListener("click", () => {
+		btn.parentElement.remove();
+		_DB.movies.splice(index, 1);
+		createFilmsList(_DB.movies, filmsBlock);
+	  });
+	});
+  }
+  createFilmsList(_DB.movies, filmsBlock);
